0d89054849b2843254f49e6e582d8f571df5bf18
Kai Cheng <Kai Cheng>
2016-09-06 19:24:27 +0800

    修复动画错误

--- showanimation2048.js
+++ showanimation2048.js
@@ -12,11 +12,11 @@ function showNumberWithAnimation(i, j, randNumber) {
         height: "100px",
         top: getPosTop(i, j),
         left: getPosLeft(i, j)
-    }, 50);
+    }, 500);
 }
 
 function showMoveAnimation(fromx, fromy, tox, toy) {
-    var numberCell = $('#number-cell'+fromx+'-'+fromy);
+    var numberCell = $('#number-cell-'+fromx+'-'+fromy);
     numberCell.animate({
         top: getPosTop(tox, toy),
         left: getPosLeft(tox, toy)
